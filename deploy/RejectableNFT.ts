70fcf36892e4b8e6b64a9253fef977b5863927bb
Miquel A. Cabot <miquel.cabot@gmail.com>
2022-10-30 21:52:38 +0100

    create two different deployment scripts

--- deploy/RejectableNFT.ts
+++ deploy/RejectableNFT.ts
@@ -1,17 +1,14 @@
-import { HardhatRuntimeEnvironment } from 'hardhat/types';
 import { DeployFunction } from 'hardhat-deploy/types';
 
-const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
-  const { deployments, getNamedAccounts } = hre;
+const func: DeployFunction = async ({
+  // @ts-ignore
+  getNamedAccounts,
+  // @ts-ignore
+  deployments
+}) => {
   const { deploy } = deployments;
 
   const { deployer } = await getNamedAccounts();
-
-  await deploy('NFT', {
-    from: deployer,
-    args: ['NFT test', 'NFT1'],
-    log: true
-  });
 
   await deploy('RejectableNFT', {
     from: deployer,
