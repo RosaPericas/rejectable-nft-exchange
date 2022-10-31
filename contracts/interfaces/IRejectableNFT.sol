9584faac89567b4deacd05b1765d069d903b37d1
Miquel A. Cabot <miquel.cabot@gmail.com>
2022-10-31 08:09:44 +0100

    add IRejectableNFT interface code

--- contracts/interfaces/IRejectableNFT.sol
+++ contracts/interfaces/IRejectableNFT.sol
@@ -5,5 +5,41 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 
 interface IRejectableNFT is IERC721 {
-  
+    /**
+     * @dev Emitted when `tokenId` token is tried to be transferred from `from` sender to `to` receiver.
+     */
+    event TransferRequest(
+        address indexed from,
+        address indexed to,
+        uint256 indexed tokenId
+    );
+
+    /**
+     * @dev Emitted when receiver `to` rejects `tokenId` transfer from `from` to `to`.
+     */
+    event RejectTransferRequest(
+        address indexed from,
+        address indexed to,
+        uint256 indexed tokenId
+    );
+
+    /**
+     * @dev Emitted when sender `from` cancels `tokenId` transfer `from` to `to`.
+     */
+    event CancelTransferRequest(
+        address indexed from,
+        address indexed to,
+        uint256 indexed tokenId
+    );
+
+    function transferableOwnerOf(uint256 tokenId)
+        external
+        view
+        returns (address);
+
+    function acceptTransfer(uint256 tokenId) external;
+
+    function rejectTransfer(uint256 tokenId) external;
+
+    function cancelTransfer(uint256 tokenId) external;
 }
